506460343008aff1a9a30f997272aac3b57aaa0c
SXSTgit <1085432162@qq.com>
2019-09-15 15:54:48 +0800

    添加验证码登陆

--- jggcommon/jgg_common_dao/src/main/java/com/zb/mapper/UserMapper.java
+++ jggcommon/jgg_common_dao/src/main/java/com/zb/mapper/UserMapper.java
@@ -7,10 +7,14 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserMapper {
 
-	//登陆
+	//密码登陆
 	public User getUser(@Param(value = "phone") String phone, @Param(value = "password") String password)throws Exception;
 
+	//验证码登陆
+
+	public User getUserBySMS(@Param(value = "phone") String phone);
 	//注册
 	public int addUser(User user)throws Exception;
+
 
 }
