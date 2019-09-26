e5b3e2d5450da46f120b52e265d5abf95a492fc0
zhansan <1160628663@qq.com>
2019-09-26 14:32:11 +0800

    后台Token登陆

---
+++ jggcommon/jgg_common_dto/src/main/java/com/zb/vo/AdminToken.java
@@ -0,0 +1,24 @@
+package com.zb.vo;
+
+import com.zb.pojo.Admin;
+
+public class AdminToken {
+    private String str;
+    private Admin admin;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+}
