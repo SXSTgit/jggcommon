e1549e5cf460acd9b2367eee8e6dc27a688f547b
SXSTgit <1085432162@qq.com>
2019-09-29 09:43:06 +0800

    添加购物车

--- jggcommon/jgg_common_dao/src/main/java/com/zb/mapper/CartMapper.java
+++ jggcommon/jgg_common_dao/src/main/java/com/zb/mapper/CartMapper.java
@@ -8,11 +8,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface CartMapper {
 
-	public Cart getCartById(@Param(value = "id") Long id)throws Exception;
+	public Cart getCartById(@Param(value = "id") String  id)throws Exception;
 
-	public List<Cart>	getCartListByMap(Map<String,Object> param)throws Exception;
+	public List<Cart>	getCartListByuserId(@Param("userId") Integer userId)throws Exception;
 
-	public Integer getCartCountByMap(Map<String,Object> param)throws Exception;
 
 	public Integer insertCart(Cart cart)throws Exception;
 
@@ -20,6 +19,6 @@ public interface CartMapper {
 
 	public Integer deleteCartById(@Param(value = "id") Long id)throws Exception;
 
-	public Integer batchDeleteCart(Map<String,List<String>> params);
+
 
 }
